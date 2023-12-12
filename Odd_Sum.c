2244c49b9f119d2e92ed07d1b33488952018398e
MANEESHKOTI <147472853+MANEESHKOTI@users.noreply.github.com>
2023-12-13 00:48:36 +0530

    #include<stdio.h> int main(){     int n,sum = 0;     scanf("%d",&n);     int arr[n];     for( int i = 0 ; i < n; i++){         scanf("%d",&arr[i]);     }     for( int j = 0 ; j < n ; j++){         if((arr[j]%2)!=0 || arr[j]==1){             sum+=arr[j];         }     }     printf("%d",sum); }

---
+++ Odd_Sum.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int n,sum = 0;
+    scanf("%d",&n);
+    int arr[n];
+    for( int i = 0 ; i < n; i++){
+        scanf("%d",&arr[i]);
+    }
+    for( int j = 0 ; j < n ; j++){
+        if((arr[j]%2)!=0 || arr[j]==1){
+            sum+=arr[j];
+        }
+    }
+    printf("%d",sum);
+}
